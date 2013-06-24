474e3493b11bcfb51f79788d3f5882a9d43ff348
Jimmy Da <jimmida@gmail.com>
2013-06-23 18:49:21 -0700

    Following other ursers functional

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ActiveRecord::Base
 	attr_accessible :avatar_url, :email, :name, :password, :password_confirmation, :username
 
 	has_many :ribbits
+	has_many :follower_relationships, classname: "Relationship", foreign_key:"followed_id"
+	has_many :followed_relationships, classname: "Relationship", foreign_key:"follower_id"
+	has_many :followers, through :follower_relationships
+	has_many :followeds, through :followed_relationships
 
   validates :name, presence: true
   validates :username, uniqueness: true, presence: true
@@ -18,4 +22,11 @@ class User < ActiveRecord::Base
   def create_avatar_url
   	self.avatar_url = "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(self.email)}?s=50"
   end
+
+  def following? user
+  	self.followeds.include? user
+  end
+
+  def follow? user
+  	Relationship.create follower_id: self.id, followed_id: user.id
 end
